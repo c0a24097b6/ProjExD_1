6c70de6619d33126cdb9c4bcc34e4deb838f5e70
Haruto Saikatsu <c0a24097b6@edu.teu.ac.jp>
2025-11-18 17:42:04 +0900

    課題2

--- pygame_image.py
+++ pygame_image.py
@@ -26,15 +26,17 @@ def main():
         screen.blit(bg_img, [-x+3200, 0])
 
         key_lst = pg.key.get_pressed()
-        kk_rct.move_ip((-1,0))
+        dx = -1
+        dy = 0
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
+            dy+=-1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0,1))
+            dy+=1
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((2,0))
+            dx+=2
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1,0))
+            dy+=-1
+        kk_rct.move_ip((dx,dy))
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1        
